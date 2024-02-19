3ea96e28c44bea007b81e159e0d410410964365c
osparking <jbpark03@gmail.com>
2024-02-20 04:58:17 +0900

    거리 관리자 - 일부 구현

--- src/main/java/space/bum/junit/jb_p4/airport/DistanceManager.java
+++ src/main/java/space/bum/junit/jb_p4/airport/DistanceManager.java
@@ -1,19 +1,30 @@
 package space.bum.junit.jb_p4.airport;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class DistanceManager {
-  public Map<Passenger, Integer> getPassengersDistancesMap() {
-    return null;
+  private static final int DISTANCE_FACTOR = 10;
+
+  private Map<Passenger, Integer> passengerDistances = new HashMap<>();
+  private Map<Passenger, Integer> passengerPoints = new HashMap<>();
+
+  public Map<Passenger, Integer> getPassengerDistances() {
+    return Collections.unmodifiableMap(passengerDistances);
   }
 
-  public Map<Passenger, Integer> getPassengersPointsMap() {
-    return null;
+  public Map<Passenger, Integer> getPassengerPoints() {
+    return Collections.unmodifiableMap(passengerPoints);
   }
 
   public void addDistance(Passenger passenger, int distance) {
   }
 
   public void calculateGivenPoints() {
+    getPassengerDistances().keySet().stream().forEach(p -> {
+      passengerPoints.put(p,
+          getPassengerDistances().get(p) / DISTANCE_FACTOR);
+    });
   }
 }
