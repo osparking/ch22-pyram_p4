6c7d8961bd3149593f1a55e613ef6acbdce1a59b
osparking <jbpark03@gmail.com>
2024-02-20 04:46:03 +0900

    탑승 거리 관리자 - 복붙

---
+++ src/main/java/space/bum/junit/jb_p4/airport/DistanceManager.java
@@ -0,0 +1,19 @@
+package space.bum.junit.jb_p4.airport;
+
+import java.util.Map;
+
+public class DistanceManager {
+  public Map<Passenger, Integer> getPassengersDistancesMap() {
+    return null;
+  }
+
+  public Map<Passenger, Integer> getPassengersPointsMap() {
+    return null;
+  }
+
+  public void addDistance(Passenger passenger, int distance) {
+  }
+
+  public void calculateGivenPoints() {
+  }
+}
